fab0d2f61e1f3508c14d41aed6f2cecf26a837a7
Irakliik <159915721+Irakliik@users.noreply.github.com>
2025-09-24 23:20:39 +0400

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+    {
+        env: {
+            browser: true,
+            node: true,
+            es2021: true,
+        },
+        rules: {
+            indent: ['error', 4],
+            quotes: ['error', 'single'],
+            semi: ['error', 'always'],
+            'linebreak-style': ['error', 'unix'],
+        },
+        files: ['**/*.{js,mjs,cjs}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: { globals: globals.browser },
+    },
+]);
