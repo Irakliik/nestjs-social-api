7ea00efe6064110847676427e2ecc25b28d42828
Irakliik <159915721+Irakliik@users.noreply.github.com>
2025-09-25 14:16:02 +0400

    add health check endpoint

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 import http from 'http';
 import dotenv from 'dotenv';
-
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const packageJSONPath = path.join(__dirname, 'package.json');
+const { version } = JSON.parse(fs.readFileSync(packageJSONPath, 'utf-8'));
 
 const port = process.env.PORT;
 
 const server = http.createServer(function (request, response) {
-    response.end('Hello World');
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('X-Powered-By', 'Node.js');
+    response.setHeader('Cache-Control', 'no-store');
+    response.setHeader('Connection', 'keep-alive');
+    response.setHeader('Date', new Date().toUTCString());
+
+    if (request.url === '/health' && request.method === 'GET') {
+        const data = { version };
+
+        response.end(JSON.stringify(data));
+    } else {
+        response.end();
+    }
 });
 
 server.listen(port);
