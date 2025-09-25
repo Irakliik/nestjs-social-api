62fa38d006805686648957978fd243706b5e41c7
Irakliik <159915721+Irakliik@users.noreply.github.com>
2025-09-25 20:42:39 +0400

    added winston logger

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ import dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import winston from 'winston';
+
 dotenv.config();
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        winston.format.printf(({ level, message, timestamp }) => {
+            return `${timestamp} - ${level} - ${message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'app.log' }),
+    ],
+});
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,18 +30,24 @@ const { version } = JSON.parse(fs.readFileSync(packageJSONPath, 'utf-8'));
 const port = process.env.PORT;
 
 const server = http.createServer(function (request, response) {
+    logger.info(`Incoming request: ${request.method} ${request.url}`);
+
     response.setHeader('Content-Type', 'application/json');
     response.setHeader('X-Powered-By', 'Node.js');
     response.setHeader('Cache-Control', 'no-store');
     response.setHeader('Connection', 'keep-alive');
     response.setHeader('Date', new Date().toUTCString());
 
-    if (request.url === '/health' && request.method === 'GET') {
-        const data = { version };
+    try {
+        if (request.url === '/health' && request.method === 'GET') {
+            const data = { version };
 
-        response.end(JSON.stringify(data));
-    } else {
-        response.end();
+            response.end(JSON.stringify(data));
+        } else {
+            response.end();
+        }
+    } catch (err) {
+        logger.error(`Error - ${err.message}`);
     }
 });
 
