0caf12f89edb39db30d9db6a0e0d344c4a9b3ef5
Irakliik <159915721+Irakliik@users.noreply.github.com>
2025-10-07 21:25:08 +0400

    modified again files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,3 @@
-// import js from '@eslint/js';
-// import globals from 'globals';
-// import { defineConfig } from 'eslint/config';
-// import tseslint from 'typescript-eslint';
-// export default defineConfig([
-//     {
-//         rules: {
-//             indent: ['error', 4],
-//             quotes: ['error', 'single'],
-//             semi: ['error', 'always'],
-//             'linebreak-style': ['error', 'unix'],
-//         },
-//         files: ['src/**/*.{ts,tsx}'],
-//         plugins: { js },
-//         extends: ['js/recommended'],
-//         languageOptions: { globals: { ...globals.node, ...globals.browser } },
-//     },
-// ]);
-
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
